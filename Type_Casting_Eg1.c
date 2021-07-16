7992ba9d5f47691085c1f5626ab7798105ad4019
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-07-16 23:48:37 +0530

    Add files via upload

---
+++ Type_Casting_Eg1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a = 15; //¤
+	float b = 202.36;
+	char c[20] = "vivek";
+	printf("%d\n" , c);
+	printf("%c\n", a);
+	printf("%d\n",(int)b);
+	return 0 ;
+	
+}
